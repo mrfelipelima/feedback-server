30729d7bd7673d63b73a384d8d4e6fa085be278e
Felipe Lima <mrfelipelima@gmail.com>
2022-05-05 08:42:36 -0300

    feat: send a notification email when a new feedback is included

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import nodemailer from "nodemailer";
 
 import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "c8061e499a991e",
+    pass: "95a4ebf856f474",
+  },
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -15,6 +25,18 @@ app.post("/feedbacks", async (req, res) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Felipe Lima <mrfelipelima@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
   });
 
   return res.status(201).json({ data: feedback });
